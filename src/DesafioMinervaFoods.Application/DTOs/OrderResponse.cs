72f2ff67ce41371348f8f1e922183774131586be
Phillipe R Souza <8622005+phillrog@users.noreply.github.com>
2026-02-12 11:34:09 -0300

    validação do pedido

---
+++ src/DesafioMinervaFoods.Application/DTOs/OrderResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace DesafioMinervaFoods.Application.DTOs
+{
+    public record OrderResponse(
+    Guid OrderId,
+    decimal TotalAmount,
+    string Status,
+    bool RequiresManualApproval);
+}
